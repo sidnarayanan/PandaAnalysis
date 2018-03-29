87e17ba06782fe0e4b600b8e096b2d39d561fadd
Dylan George Hsu <dghsu@mit.edu>
2018-03-29 19:31:41 -0400

    use the smeared pt in all cases

--- Flat/src/ModulesBTagSFs.cc
+++ Flat/src/ModulesBTagSFs.cc
@@ -186,7 +186,7 @@ void PandaAnalyzer::JetCMVAWeights()
   jetFlavors.reserve(nJ);
   for (unsigned int iJ=0; iJ!=nJ; ++iJ) {
     const panda::Jet *jet = bCandJets.at(iJ);
-    jetPts.push_back(jet->pt());
+    jetPts.push_back(jet->ptSmear);
     jetEtas.push_back(jet->eta());
     jetCSVs.push_back(jet->csv);
     jetCMVAs.push_back(jet->cmva);
