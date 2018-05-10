9d74e0b3548009b6aaaed8352640ff9e8dbfe737
Dylan George Hsu <dghsu@mit.edu>
2018-05-10 16:28:09 -0400

    Trigger safe EMU preselection included for Hbb 2 lepton

--- Flat/src/Selection.cc
+++ Flat/src/Selection.cc
@@ -100,7 +100,12 @@ bool VHbbSel::do_accept() const
   // ZllHbb
   if (bestJet1>25 && bestJet2>25 && 
       ((gt->nLooseElectron>1 && gt->electronPt[0]>20 && gt->electronPt[1]>15) ||
-       (gt->nLooseMuon>1 && gt->muonPt[0]>20 && gt->muonPt[1]>10)) &&
+       (gt->nLooseMuon>1 && gt->muonPt[0]>20 && gt->muonPt[1]>10) ||
+       (gt->nLooseMuon>0 && gt->nLooseElectron>0 && (
+         (gt->electronPt[0]>25 && gt->muonPt[0]>10) ||
+         (gt->electronPt[0]>10 && gt->muonPt[0]>25) 
+       ))
+      ) &&
       (gt->hbbpt[0]>50 || gt->nFatjet>0))
   {
     return true;
