af4f66174195c7fc62ef277f7ead409aa65b3262
Sid Narayanan <sidn@mit.edu>
2018-07-06 14:40:17 -0400

    fixing merge error

--- Flat/src/Selection.cc
+++ Flat/src/Selection.cc
@@ -88,23 +88,23 @@ bool VHbbSel::do_accept() const
   }
 
   // WlnHbb
-  if ((gt->nTightElectron>0 && gt->electronPt[0]>30) ||
+  if (((gt->nTightElectron>0 && gt->electronPt[0]>30) ||
        (gt->nTightMuon>0 && gt->muonPt[0]>25)) &&
-      gt->topWBosonPt>50 &&
-      (gt->hbbpt[0]>50 || gt->nFatJet>0) 
+        gt->topWBosonPt>50 &&
+        (gt->hbbpt[0]>50 || gt->nFatJet>0)) 
   {
     return true;
   }
 
   // zllhbb
-  if ((gt->nLooseElectron>1 && gt->electronPt[0]>20 && gt->electronPt[1]>15) ||
+  if (((gt->nLooseElectron>1 && gt->electronPt[0]>20 && gt->electronPt[1]>15) ||
        (gt->nLooseMuon>1 && gt->muonPt[0]>20 && gt->muonPt[1]>10) ||
        (gt->nLooseMuon>0 && gt->nLooseElectron>0 && (
          (gt->electronPt[0]>25 && gt->muonPt[0]>10) ||
          (gt->electronPt[0]>10 && gt->muonPt[0]>25) 
        ))
       ) &&
-      (gt->hbbpt[0]>50 || gt->nFatJet>0)
+      (gt->hbbpt[0]>50 || gt->nFatJet>0))
   {
     return true;
   }
