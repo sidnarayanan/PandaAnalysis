40ef5df9a3fea9a517c9303626157ccc1ebc8957
Sid Narayanan <sidn@mit.edu>
2018-07-23 13:36:44 -0400

    breg things

--- VBF/triggers/add_eff.py
+++ VBF/triggers/add_eff.py
@@ -5,7 +5,6 @@ import argparse
 
 parser = argparse.ArgumentParser(description='')
 parser.add_argument('--histfile',type=str)
-parser.add_argument('--histname',type=str)
 parser.add_argument('--outbranch',type=str,default='sf_l1')
 parser.add_argument('outfile',metavar='outfile',type=str,nargs='+',help='input file(s) to process')
 args = parser.parse_args()
@@ -19,31 +18,44 @@ import PandaCore.Tools.Functions
 
 Load('PandaCoreTools')
  
-hba = root.H2BranchAdder()
+hba = { 'smu' : root.H2BranchAdder(),
+        'jet' : root.H2BranchAdder() }
 hfile = root.TFile.Open(args.histfile)
 ptformula = 'jot%iPt'
 etaformula = 'fabs(jot%iEta)'
-hbranch = args.outbranch+'_jot%i'
-h = hfile.Get(args.histname)
-hba.setH(h)
+hbranch = 'sf_l1%s_jot%i'
+hsmu = hfile.Get('h_SingleMuon_spike_finor_pteta')
+hjet = hfile.Get('h_JetHT_spike_finor_pteta')
+hba['smu'].setH(hsmu)
+hba['jet'].setH(hjet)
 
 fba = root.FormulaBranchAdder()
-fba.newBranchName = args.outbranch
-fba.formula = '(1-{0}_jot1)*(1-{0}_jot2)'.format(args.outbranch)
+#formula = 'TMath::Max(0,TMath::Min(1,(1-{0}*((sf_l1smu_jot1*(jot1Pt<300)) + (sf_l1jet_jot1*(jot1Pt>300))))*(1-{0}*((sf_l1smu_jot1*(jot1Pt<300)) + (sf_l1jet_jot1*(jot1Pt>300))))))'
+formula = '(1-sf_l1smu_jot1)*(1-sf_l1smu_jot2)'
+#fba.formula = '(1-{0}_jot1)*(1-{0}_jot2)'.format(args.outbranch)
 #fba.formula = '((1-sf_l1_jot1) * (1-sf_l1_jot2))'
 
 for fpath in args.outfile:
     f = root.TFile.Open(fpath,'update')
     t = f.Get('events')
     for i in [1,2]:
-        hba.formulaX = ptformula%i
-        hba.formulaY = etaformula%i 
-        hba.newBranchName = hbranch%i 
-        hba.addBranch(t)
+        for pd in ['smu','jet']:
+            hba[pd].formulaX = ptformula%i
+            hba[pd].formulaY = etaformula%i 
+            hba[pd].newBranchName = hbranch%(pd,i)
+            hba[pd].addBranch(t)
     f.WriteTObject(t, 'events', 'overwrite')
     f.Close()
     f = root.TFile.Open(fpath,'update')
     t = f.Get('events')
+    fba.newBranchName = args.outbranch
+    fba.formula = formula.format(1)
+    fba.addBranch(t)
+    fba.newBranchName = args.outbranch+'_Up'
+    fba.formula = formula.format(1.2)
+    fba.addBranch(t)
+    fba.newBranchName = args.outbranch+'_Down'
+    fba.formula = formula.format(0.8)
     fba.addBranch(t)
     f.WriteTObject(t, 'events', 'overwrite')
     f.Close()
